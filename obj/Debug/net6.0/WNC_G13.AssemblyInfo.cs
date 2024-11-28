31cf8d282eac7ff8971e6ede7be4599a5d28c01d
anhduc2204 <101176957+anhduc2204@users.noreply.github.com>
2024-11-28 09:30:05 +0700

    merge remote repository

--- obj/Debug/net6.0/WNC_G13.AssemblyInfo.cs
+++ obj/Debug/net6.0/WNC_G13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WNC_G13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28e336af2b45bd41b194ccd6ae16f5802a25895")]
 [assembly: System.Reflection.AssemblyProductAttribute("WNC_G13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WNC_G13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
